ff655521c6ec379a627455a211c13e8d6f815c5c
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-05-03 18:15:14 -0400

    configuration of the component gifs-page

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GifsPageComponent } from './gifs-page/gifs-page.component';
 
-
-
 @NgModule({
-  declarations: [
-    GifsPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [GifsPageComponent],
+  exports: [GifsPageComponent],
+  imports: [CommonModule],
 })
-export class GifsModule { }
+export class GifsModule {}
