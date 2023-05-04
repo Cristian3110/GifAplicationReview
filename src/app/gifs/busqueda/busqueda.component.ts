fb6733b8944d02a3e9b0959088b1b0b3ac419afd
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-05-04 18:04:49 -0400

    exercise with ViewChild to get refrence to object of the HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css'],
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  // buscando el elemento
+  // Non-null assertion operator (!)
+  // importarlo cunado te marque y sea un tipo generico
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  buscar(termino: string) {
-    console.log(termino);
+  //? Ejercicio para demostrar con el @ViewChild la manipulación del HTML
+  buscar() {
+    const value = this.txtBuscar.nativeElement.value;
+
+    console.log(value);
+    // para limpiar
+    this.txtBuscar.nativeElement.value = '';
   }
 }
